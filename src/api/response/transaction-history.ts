efef974dc449e1283aabca1c9522ff792d6c6285
jinoosss <jwchoi@onbloc.xyz>
2022-11-22 18:55:41 +0900

    feat: fetch transaction history pagable and set for multi token support

--- src/api/response/transaction-history.ts
+++ src/api/response/transaction-history.ts
@@ -1,25 +1,27 @@
+export interface HistoryItem {
+  height: number;
+  date: string;
+  hash: string;
+  result: {
+    status: string;
+    reason: string;
+  };
+  type: string;
+  from?: string;
+  to?: string;
+  func: string;
+  send: {
+    value: number;
+    denom: string;
+  };
+  fee: {
+    value: number;
+    denom: string;
+  };
+}
+
 export interface History {
   hits: number;
   next: boolean;
-  txs: Array<{
-    height: number;
-    date: string;
-    hash: string;
-    result: {
-      status: string;
-      reason: string;
-    };
-    type: string;
-    from?: string;
-    to?: string;
-    func: string;
-    send: {
-      value: number;
-      denom: string;
-    };
-    fee: {
-      value: number;
-      denom: string;
-    };
-  }>;
+  txs: Array<HistoryItem>;
 }
