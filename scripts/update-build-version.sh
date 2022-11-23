80d8f2bd7f24bec34380a0f88a56c29802caae6a
jinoosss <jwchoi@onbloc.xyz>
2022-11-23 15:08:03 +0900

    chore: change qa build version update script

--- scripts/update-build-version.sh
+++ scripts/update-build-version.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+if [ "$branch" != "qa" ]; then
+  echo "current brunch is not 'qa'"
+  exit;
+fi
+
 # setup qa build version
 today=$(date "+%Y%m%d")
 build_info_file="scripts/build.info"
 build_count=1
+
 if [ -f $build_info_file ]; then
+
   current_build_version=`cat ./scripts/build.info`
   qa_build_version_prefix=$today-;
   if [[ $current_build_version == $qa_build_version_prefix* ]]; then
     build_count=$(sed "s/$qa_build_version_prefix//g" <<< "$current_build_version")
     build_count=$((build_count + 1))
   fi
-else 
+
+else
   touch $build_info_file
 fi
+
 build_version=$today-$build_count
 
 # overwrite build.info
 echo $build_version > $build_info_file
+
+# git push
+git add scripts/build.info
+git commit -m "chore: update qa version '$build_version'"
+git push
