c2206859f3b50e6c73cc7a31ec6331fc5f788425
jinoosss <jwchoi@onbloc.xyz>
2023-02-23 21:08:38 +0900

    feat: update background functions

--- packages/adena-extension/src/components/background/background.tsx
+++ packages/adena-extension/src/components/background/background.tsx
@@ -1,30 +1,46 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useTransactionHistory } from "@hooks/use-transaction-history";
 import { useRecoilState, useResetRecoilState } from "recoil";
 import { CommonState, ExploreState, GnoClientState, WalletState } from "@states/index";
 import { createImageDataBySvg } from "@common/utils/client-utils";
-import { AdenaStorage } from "@common/storage";
 import { useLocation } from "react-router-dom";
 import { GnoClient } from "gno-client";
 import { useAdenaContext } from "@hooks/use-context";
+import { useWalletBalances } from "@hooks/use-wallet-balances";
+import { useLoadAccounts } from "@hooks/use-load-accounts";
 
 interface Props {
     children?: React.ReactNode;
 }
 
 export const Background = ({ children }: Props) => {
-    const { accountService, chainService, tokenService } = useAdenaContext();
     const location = useLocation();
-    const [currentAccountAddress, setCurrentAccountAddress] = useState<string>('');
+    const { tokenService } = useAdenaContext();
     const [gnoClient] = useRecoilState(GnoClientState.current);
     const [transactionHistory] = useRecoilState(WalletState.transactionHistory)
     const [, updateLastTransactionHistory] = useTransactionHistory();
     const [exploreSites, setExploreSites] = useRecoilState(ExploreState.sites);
     const [, setFailedNetwork] = useRecoilState(CommonState.failedNetwork);
+    const [currentAccount] = useRecoilState(WalletState.currentAccount);
+    const [currentBalance] = useRecoilState(WalletState.currentBalance);
+    const [, updateBalances] = useWalletBalances(gnoClient);
+    const [state, setState] = useRecoilState(WalletState.state);
+    const [accounts] = useRecoilState(WalletState.accounts);
+    const { updateAccountBalances } = useLoadAccounts();
 
     const clearTransactionHistory = useResetRecoilState(WalletState.transactionHistory);
     const clearCurrentBalance = useResetRecoilState(WalletState.currentBalance);
     const clearHistoryPosition = useResetRecoilState(CommonState.historyPosition);
+
+    useEffect(() => {
+        if (state === "LOADING" || !currentBalance) {
+            if (!accounts || accounts.length === 0) {
+                setState("NONE");
+                return;
+            }
+            updateBalances();
+        }
+    }, [location]);
 
     useEffect(() => {
         if (gnoClient) {
@@ -32,19 +48,35 @@ export const Background = ({ children }: Props) => {
         }
     }, [gnoClient?.chainId, location]);
 
+    useEffect(() => {
+        if (!accounts || accounts.length === 0) {
+            return;
+        }
+        updateBalances();
+        updateAccountBalances();
+    }, [accounts?.length, currentAccount?.getAddress(), gnoClient?.chainId]);
+
+    useEffect(() => {
+        if (!accounts || accounts.length === 0) {
+            return;
+        }
+        const balancesFetchTimer = setInterval(() => {
+            updateAccountBalances();
+        }, 5000);
+        return () => { clearInterval(balancesFetchTimer) }
+    }, [gnoClient?.chainId, accounts?.length]);
+
     /**
      * History Data Interval Fetch
      */
     useEffect(() => {
-        accountService.getCurrentAccount().then(currentAccount => {
-            if (gnoClient?.chainId && currentAccount) {
-                const historyFetchTimer = setInterval(() => {
-                    updateLastTransactionHistory();
-                }, 5000);
-                return () => { clearInterval(historyFetchTimer) }
-            }
-        });
-    }, [gnoClient?.chainId, transactionHistory.currentPage]);
+        if (gnoClient?.chainId && currentAccount) {
+            const historyFetchTimer = setInterval(() => {
+                updateLastTransactionHistory();
+            }, 5000);
+            return () => { clearInterval(historyFetchTimer) }
+        }
+    }, [gnoClient?.chainId, currentAccount?.getAddress(), transactionHistory.currentPage]);
 
 
     useEffect(() => {
@@ -87,6 +119,7 @@ export const Background = ({ children }: Props) => {
 
     const updateAppInfos = async () => {
         if (exploreSites.length > 0) {
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
             const convertedSites: Array<any> = [];
             for (const exploreSite of exploreSites) {
                 const logo = await createImageDataBySvg(exploreSite.logo);
