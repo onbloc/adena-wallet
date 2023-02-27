58dc5c5c3353ec3702a39ea5cfe1709d56ec506a
jinoosss <jwchoi@onbloc.xyz>
2023-02-28 00:47:27 +0900

    fix: typo resource params

--- packages/adena-extension/src/components/icons/icon-assets/icon-connect-loading.tsx
+++ packages/adena-extension/src/components/icons/icon-assets/icon-connect-loading.tsx
@@ -33,8 +33,8 @@ const IconConnectLoading = ({ className }: { className: string }) => {
     >
       <rect width='100' height='100' rx='50' fill='#191920' />
       <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
+        fillRule='evenodd'
+        clipRule='evenodd'
         d='M30.9995 69.0016C41.4932 79.4953 58.5068 79.4953 69.0005 69.0016C79.4942 58.5079 79.4942 41.4942 69.0005 31.0005C58.5068 20.5068 41.4932 20.5068 30.9995 31.0005C20.5058 41.4942 20.5058 58.5079 30.9995 69.0016ZM40.2283 59.7722C45.6251 65.1689 54.3749 65.1689 59.7717 59.7722C65.1685 54.3754 65.1685 45.6255 59.7717 40.2288C54.3749 34.832 45.6251 34.832 40.2283 40.2288C34.8315 45.6255 34.8315 54.3754 40.2283 59.7722Z'
         fill='url(#paint0_linear_4554_105222)'
       />
@@ -48,8 +48,8 @@ const IconConnectLoading = ({ className }: { className: string }) => {
           y2='43.1492'
           gradientUnits='userSpaceOnUse'
         >
-          <stop stop-color='#0059FF' />
-          <stop offset='1' stop-color='#0058C9' stop-opacity='0' />
+          <stop stopColor='#0059FF' />
+          <stop offset='1' stopColor='#0058C9' stopOpacity='0' />
         </linearGradient>
       </defs>
     </SVG>
