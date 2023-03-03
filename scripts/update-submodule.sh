ea7c851c76e1fd625094c5a8f02918b7fb153e8d
jinoosss <jwchoi@onbloc.xyz>
2023-03-03 18:27:11 +0900

    chore: update qa version '20230303-1'

--- scripts/update-submodule.sh
+++ scripts/update-submodule.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-git submodule deinit packages/adena-torus-signin
+git submodule deinit packages/adena-torus-signin <<!
+yes
+!
 git rm --cached packages/adena-torus-signin
 rm -rf .git/modules/packages/adena-torus-signin
 rm -rf packages/adena-torus-signin
 
-git submodule add git@github.com:onbloc/adena-torus-signin.git packages/adena-torus-signin <<!       
+git submodule add git@github.com:onbloc/adena-torus-signin.git packages/adena-torus-signin <<!
 yes
 !
