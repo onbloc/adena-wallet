e255ac9f8cfc232d9cbb038cc79046fb82db3d37
jinoosss <jwchoi@onbloc.xyz>
2023-03-03 18:25:02 +0900

    dev: add script change submodule repository

--- scripts/update-submodule.sh
+++ scripts/update-submodule.sh
@@ -4,4 +4,6 @@ git rm --cached packages/adena-torus-signin
 rm -rf .git/modules/packages/adena-torus-signin
 rm -rf packages/adena-torus-signin
 
-git submodule add git@github.com:onbloc/adena-torus-signin.git packages/adena-torus-signin
+git submodule add git@github.com:onbloc/adena-torus-signin.git packages/adena-torus-signin <<!       
+yes
+!
