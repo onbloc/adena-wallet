9b5f1489707fdb7182d13aa4c0ce565033dcee4b
hyojinDev <gywls3394@gmail.com>
2023-03-03 15:11:43 +0900

    fix: approve transaction ui

--- packages/adena-extension/src/pages/wallet/approve-sign/approve-main-loading.tsx
+++ packages/adena-extension/src/pages/wallet/approve-sign/approve-main-loading.tsx
@@ -2,24 +2,6 @@ import theme from '@styles/theme';
 import { Circle, Round, Spinner } from '@components/loadings';
 import Text from '@components/text';
 import React from 'react';
-import styled from 'styled-components';
-
-interface LoadingProps {
-  loading: boolean;
-}
-
-const Wrapper = styled.div`
-  ${({ theme }) => theme.mixins.flexbox('column', 'center', 'flex-start')};
-  position: relative;
-  width: 100%;
-  height: 492px;
-  padding-top: 24px;
-  .absolute-buttons {
-    position: absolute;
-    bottom: 24px;
-    padding: 0px 20px;
-  }
-`;
 
 const ApproveMainLoading = () => {
   return (
