731f64a22e4fade6aeefac95b93bbceade308937
jinoosss <jwchoi@onbloc.xyz>
2023-04-19 17:45:29 +0900

    feat: [ADN-22] Create a token search component

---
+++ packages/adena-extension/src/components/manage-token/manage-token-search-input/manage-token-search-input.spec.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import { render } from '@testing-library/react';
+import theme from '@styles/theme';
+import { GlobalStyle } from '@styles/global-style';
+import ManageTokenSearchInput, { ManageTokenSearchInputProps } from './manage-token-search-input';
+
+describe('ManageTokenSearchInput Component', () => {
+  it('ManageTokenSearchInput render', () => {
+    const args: ManageTokenSearchInputProps = {
+      keyword: "as",
+      onChangeKeyword: () => { return; },
+      onClickAdded: () => { return; }
+    };
+
+    render(
+      <RecoilRoot>
+        <GlobalStyle />
+        <ThemeProvider theme={theme}>
+          <ManageTokenSearchInput {...args} />
+        </ThemeProvider>
+      </RecoilRoot>,
+    );
+  });
+});
---
+++ packages/adena-extension/src/components/manage-token/manage-token-search-input/manage-token-search-input.styles.ts
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+export const ManageTokenSearchInputWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 48px;
+  padding: 12px 16px;
+  align-items: center;
+  justify-content: flex-start;
+  border-radius: 30px;
+  border: 1px solid ${({ theme }) => theme.color.neutral[6]};
+
+  .search-icon-wrapper {
+    display: inline-flex;
+    flex-shrink: 0;
+    width: fit-content;
+    align-items: center;
+    padding: 0 5px;
+
+    .search {
+      width: 17px;
+      height: 17px;
+    }
+  }
+
+  .input-wrapper {
+    display: inline-flex;
+    flex-shrink: 1;
+    width: 100%;
+    height: 24px;
+    padding: 0 12px;
+
+    .search-input {
+      width: 100%;
+      ${({ theme }) => theme.fonts.body2Reg};
+    }
+  }
+
+  .added-icon-wrapper {
+    display: inline-flex;
+    flex-shrink: 0;
+    width: 24px;
+    height: 24px;
+    align-items: center;
+    cursor: pointer;
+
+    .added {
+      width: 100%;
+      height: 100%;
+    }
+  }
+`;
