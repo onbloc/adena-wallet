0011408e4a0e39c7076a13cb268b63ff269c72bd
jinoosss <jwchoi@onbloc.xyz>
2023-04-30 01:52:21 +0900

    fix: [ADN-131] Fix smooth screen panning when executing Remove Account

--- packages/adena-extension/src/hooks/use-remove-account.tsx
+++ packages/adena-extension/src/hooks/use-remove-account.tsx
@@ -1,15 +1,12 @@
 import { useWalletContext } from './use-context';
 import { Account } from 'adena-module';
 import { useCurrentAccount } from './use-current-account';
-import { useRecoilState } from 'recoil';
-import { WalletState } from '@states/index';
 
 export const useRemoveAccount = (): {
   availRemoveAccount: () => Promise<boolean>,
   removeAccount: (account: Account) => Promise<boolean>,
 } => {
   const { wallet, updateWallet } = useWalletContext();
-  const [, setState] = useRecoilState(WalletState.state);
   const { changeCurrentAccount } = useCurrentAccount();
 
   const availRemoveAccount = async () => {
@@ -21,14 +18,12 @@ export const useRemoveAccount = (): {
     if (!wallet) {
       return false;
     }
-    setState('LOADING');
     const clone = wallet.clone();
     clone.removeAccount(account);
     const nextAccount = clone.accounts[clone.accounts.length - 1];
     clone.currentAccountId = nextAccount.id;
-    await changeCurrentAccount(nextAccount);
     await updateWallet(clone);
-    setState('FINISH');
+    await changeCurrentAccount(nextAccount);
     return true;
   };
 
