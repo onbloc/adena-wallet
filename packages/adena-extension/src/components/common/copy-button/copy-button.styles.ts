37e8eb04b69afb19e98e89fcdb93401e6ea69353
jinoosss <jwchoi@onbloc.xyz>
2023-07-20 15:59:52 +0900

    feat: Modify the transaction details page

---
+++ packages/adena-extension/src/components/common/copy-button/copy-button.styles.ts
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+interface CopyButtonWrapperProps {
+  checked: boolean;
+}
+
+export const CopyButtonWrapper = styled.div<CopyButtonWrapperProps>`
+  display: inline-flex;
+  flex-direction: column;
+  width: 16px;
+  height: 16px;
+  cursor: pointer;
+
+  svg {
+    path {
+      transition: 0.2s;
+      stroke: ${({ theme, checked }) =>
+        checked ? theme.color.neutral[0] : theme.color.neutral[9]};
+    }
+  }
+
+  :hover svg {
+    path {
+      stroke: ${({ theme }) => theme.color.neutral[0]};
+    }
+  }
+`;
