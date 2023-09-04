d63b4cd98646fcf601f604e0bec8a455ed6bbb2b
jinoosss <112360739+jinoosss@users.noreply.github.com>
2023-09-04 10:17:24 +0900

    fix: Add network change injection method (#259)

--- packages/adena-extension/src/components/approve/approve-changing-network/approve-changing-network-item/approve-changing-network-item.styles.ts
+++ packages/adena-extension/src/components/approve/approve-changing-network/approve-changing-network-item/approve-changing-network-item.styles.ts
@@ -15,17 +15,28 @@ export const ApproveChangingNetworkItemWrapper = styled.div`
     margin-bottom: 15px;
   }
 
-  .chain-name {
+  .chain-name-wrapper {
     display: flex;
     width: 100px;
     padding: 5px 8px;
     background-color: ${({ theme }) => theme.color.neutral[8]};
-    color: ${({ theme }) => theme.color.neutral[0]};
-    ${({ theme }) => theme.fonts.body2Reg};
-    font-weight: 500;
     border-radius: 8px;
     text-align: center;
     justify-content: center;
-    word-break: break-word;
+
+    .chain-name {
+      display: -webkit-box;
+      width: 100%;
+      color: ${({ theme }) => theme.color.neutral[0]};
+      ${({ theme }) => theme.fonts.body2Reg};
+      font-weight: 500;
+      text-align: center;
+      justify-content: center;
+      word-break: break-word;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      -webkit-line-clamp: 3;
+      -webkit-box-orient: vertical;
+    }
   }
 `;
