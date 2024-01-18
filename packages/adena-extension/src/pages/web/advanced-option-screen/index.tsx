246648901c3646d703cbd1138c733935a7c47ac4
jinoosss <jwchoi@onbloc.xyz>
2024-01-18 11:20:46 +0900

    feat: Add a page components

---
+++ packages/adena-extension/src/pages/web/advanced-option-screen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { WebMain } from '@components/atoms';
+
+const AdvancedOptionScreen: React.FC = () => {
+
+  return (
+    <WebMain>
+    </WebMain>
+  );
+};
+
+export default AdvancedOptionScreen;
---
+++ packages/adena-extension/src/pages/web/wallet-create-success-screen/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { WebMain } from '@components/atoms';
+
+const WalletCreateSuccessScreen: React.FC = () => {
+
+  return (
+    <WebMain>
+    </WebMain>
+  );
+};
+
+export default WalletCreateSuccessScreen;
