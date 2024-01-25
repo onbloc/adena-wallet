5ce2e81948eee035e895e02f005b7744f71737e2
jinoosss <jwchoi@onbloc.xyz>
2024-01-25 18:29:53 +0900

    feat: [ADN-367] Implement private key and seed phrase export

--- packages/adena-extension/src/pages/web/wallet-export-screen/index.tsx
+++ packages/adena-extension/src/pages/web/wallet-export-screen/index.tsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import { WebMain } from '@components/atoms';
+import useWalletExportScreen from '@hooks/web/wallet-export/use-wallet-export-screen';
+import { WebMainAccountHeader } from '@components/pages/web/main-account-header';
+import WalletExportInitStep from './init-step';
+import WalletExportCheckPassword from './check-password';
+import WalletExportResult from './result';
 
 const WalletExportScreen: React.FC = () => {
+  const {
+    currentAccount,
+    exportType,
+    walletExportState,
+    exportData,
+    backStep,
+    initWalletExport,
+    checkPassword,
+    moveExport,
+  } = useWalletExportScreen();
+
+  const onClickGoBack = useCallback(() => {
+    backStep();
+  }, [backStep]);
 
   return (
     <WebMain>
+      <WebMainAccountHeader
+        account={currentAccount}
+        onClickGoBack={onClickGoBack}
+      />
+      {walletExportState === 'INIT' && (
+        <WalletExportInitStep
+          exportType={exportType}
+          initWalletExport={initWalletExport}
+        />
+      )}
+      {walletExportState === 'CHECK_PASSWORD' && (
+        <WalletExportCheckPassword
+          exportType={exportType}
+          checkPassword={checkPassword}
+          moveExport={moveExport}
+        />
+      )}
+      {walletExportState === 'RESULT' && (
+        <WalletExportResult
+          exportType={exportType}
+          exportData={exportData}
+        />
+      )}
     </WebMain>
   );
 };
