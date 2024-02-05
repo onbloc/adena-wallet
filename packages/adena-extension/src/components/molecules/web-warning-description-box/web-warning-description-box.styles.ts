ce8929c302299ed9432156bb721e4471007860c3
jinoosss <jwchoi@onbloc.xyz>
2024-02-05 14:34:57 +0900

    fix: [ADN-380] Fix Airgap Account on Add Account

--- packages/adena-extension/src/components/molecules/web-warning-description-box/web-warning-description-box.styles.ts
+++ packages/adena-extension/src/components/molecules/web-warning-description-box/web-warning-description-box.styles.ts
@@ -5,6 +5,5 @@ export const StyledContainer = styled(View)`
   gap: 8px;
   align-items: flex-start;
   border-radius: 12px;
-  border: 1px solid rgba(251, 194, 36, 0.08);
-  background: rgba(251, 194, 36, 0.04);
+  background: rgba(251, 194, 36, 0.08);
 `;
