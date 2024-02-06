265c3f4943fbe3e5d1937e42ebba265a9d25c9bb
jinoosss <112360739+jinoosss@users.noreply.github.com>
2024-02-06 15:40:00 +0900

    feat: [ADN-411] Replace Your Wallet is ready page with the initial setup page (#380)

--- packages/adena-extension/src/background.ts
+++ packages/adena-extension/src/background.ts
@@ -3,7 +3,7 @@ import { MessageHandler } from './inject/message';
 chrome.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     chrome.tabs.create({
-      url: chrome.runtime.getURL('/install.html'),
+      url: chrome.runtime.getURL('/register.html'),
     });
   } else if (details.reason === 'update') {
     console.log('update');
