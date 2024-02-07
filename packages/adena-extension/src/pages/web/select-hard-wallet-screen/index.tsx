360c24de949f4bb2126fc4777f9af09b804f676a
jinoosss <jwchoi@onbloc.xyz>
2024-02-07 17:46:45 +0900

    fix: [ADN-447] Fix UI of Select Your Hardware Wallet page

--- packages/adena-extension/src/pages/web/select-hard-wallet-screen/index.tsx
+++ packages/adena-extension/src/pages/web/select-hard-wallet-screen/index.tsx
@@ -1,19 +1,38 @@
-import { ReactElement } from 'react';
+import { ReactElement, useCallback } from 'react';
 
-import { Row, View, WebImg, WebMain } from '@components/atoms';
+import { Pressable, Row, View, WebImg, WebMain, WebText } from '@components/atoms';
 import useAppNavigate from '@hooks/use-app-navigate';
 import { RoutePath } from '@types';
 
 import IconWallet from '@assets/web/hard-wallet-rounded.svg';
 import IconLedger from '@assets/web/ledger-xs.svg';
 import IconKeystone from '@assets/web/keystone-xs.svg';
+import IconLink from '@assets/web/link.svg';
 
 import WebMainButton from '@components/atoms/web-main-button';
 import { WebTitleWithDescription } from '@components/molecules';
 import { WebMainHeader } from '@components/pages/web/main-header';
+import useLink from '@hooks/use-link';
+import { HARDWARE_WALLET_LEARN_PAGE } from '@common/constants/resource.constant';
+import styled, { useTheme } from 'styled-components';
+
+const StyledLinkWrapper = styled(Row)`
+  gap: 6px;
+  align-items: flex-start;
+
+  & > * {
+    color: #6c717a;
+  }
+`;
 
 const SelectHardWalletScreen = (): ReactElement => {
+  const theme = useTheme();
   const { navigate } = useAppNavigate();
+  const { openLink } = useLink();
+
+  const moveLearnMore = useCallback(() => {
+    openLink(HARDWARE_WALLET_LEARN_PAGE);
+  }, [openLink]);
 
   return (
     <WebMain spacing={272}>
@@ -28,6 +47,7 @@ const SelectHardWalletScreen = (): ReactElement => {
         <WebTitleWithDescription
           title='Select Your Hardware Wallet'
           description='Choose one of the supported hardware wallets from the list below.'
+          marginBottom={12}
         />
       </View>
       <Row style={{ columnGap: 12 }}>
@@ -51,6 +71,14 @@ const SelectHardWalletScreen = (): ReactElement => {
           }}
         />
       </Row>
+      <Pressable onClick={moveLearnMore}>
+        <StyledLinkWrapper>
+          <WebText type='title5' color={theme.webNeutral._600}>
+            Learn More
+          </WebText>
+          <WebImg src={IconLink} size={16} />
+        </StyledLinkWrapper>
+      </Pressable>
     </WebMain>
   );
 };
