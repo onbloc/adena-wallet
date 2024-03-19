ae69101c3c443a93da612e34ae9394a6e5432f82
jinoosss <jwchoi@onbloc.xyz>
2024-03-19 16:13:05 +0900

    fix: Fix styles

--- packages/adena-extension/src/components/pages/main/main-token-balance/main-token-balance.styles.ts
+++ packages/adena-extension/src/components/pages/main/main-token-balance/main-token-balance.styles.ts
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 export const MainTokenBalanceWrapper = styled(View)`
   width: 100%;
-  height: 100px;
+  height: auto;
   align-items: center;
   justify-content: center;
 `;
