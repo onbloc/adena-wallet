e797873aa5864e8948c66a57a91f806cd9e12f21
dongwon8247 <jjj8247@gmail.com>
2024-04-09 11:37:14 +0900

    fix: Change url

--- packages/adena-extension/src/common/constants/resource.constant.ts
+++ packages/adena-extension/src/common/constants/resource.constant.ts
@@ -3,4 +3,4 @@ export const ADENA_HELP_PAGE = 'https://docs.adena.app/resources/faq' as const;
 export const ADENA_DOCS_PAGE = 'https://docs.adena.app' as const;
 export const ADENA_SETUP_AIRGAP_HELP_PAGE = 'https://docs.adena.app/user-guide/sign-in/set-up-air-gap-account' as const;
 export const HARDWARE_WALLET_LEARN_PAGE = 'https://docs.adena.app/user-guide/sign-in/connect-hardware-wallet' as const;
-export const GNO_CLI_HELP_PAGE = 'https://docs.gno.land/getting-started/working-with-key-pairs' as const;
+export const GNO_CLI_HELP_PAGE = 'https://docs.gno.land/getting-started/local-setup/working-with-key-pairs' as const;
