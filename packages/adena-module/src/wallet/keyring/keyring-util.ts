18da7ea5f15857b9dc8ede8ca63ff0f4c352a3ee
jinoosss <112360739+jinoosss@users.noreply.github.com>
2024-07-15 18:55:20 +0900

    refactor: remove legacy clients (#537)

--- packages/adena-module/src/wallet/keyring/keyring-util.ts
+++ packages/adena-module/src/wallet/keyring/keyring-util.ts
@@ -5,14 +5,7 @@ import { LedgerKeyring } from './ledger-keyring';
 import { PrivateKeyKeyring } from './private-key-keyring';
 import { Web3AuthKeyring } from './web3-auth-keyring';
 import { Tx, Wallet as Tm2Wallet } from '@gnolang/tm2-js-client';
-import { Wallet as Tm2WalletLegacy } from '@gnolang/tm2-js-client-legacy';
 import { Document, documentToTx, decodeTxMessages } from './../../utils/messages';
-import {
-  documentToTx as documentToTxLegacy,
-  decodeTxMessages as decodeTxMessagesLegacy,
-} from './../../utils/messages-legacy';
-
-const LEGACY_NETWORKS = ['test3'];
 
 export function isHDWalletKeyring(keyring: Keyring): keyring is HDWalletKeyring {
   return keyring.type === 'HD_WALLET';
@@ -49,17 +42,13 @@ export function hasPrivateKey(
   return false;
 }
 
-export function useTm2Wallet(document: Document): typeof Tm2Wallet | typeof Tm2WalletLegacy {
-  if (LEGACY_NETWORKS.includes(document.chain_id)) {
-    return Tm2WalletLegacy;
-  }
+export function useTm2Wallet(document: Document): typeof Tm2Wallet {
   return Tm2Wallet;
 }
 
-export function makeSignedTx(wallet: Tm2Wallet | Tm2WalletLegacy, document: Document): Promise<Tx> {
-  const isLegacy = LEGACY_NETWORKS.includes(document.chain_id);
-  const tx = isLegacy ? documentToTxLegacy(document) : documentToTx(document);
-  const decodeTxMessageFunction = isLegacy ? decodeTxMessagesLegacy : decodeTxMessages;
+export function makeSignedTx(wallet: Tm2Wallet, document: Document): Promise<Tx> {
+  const tx = documentToTx(document);
+  const decodeTxMessageFunction = decodeTxMessages;
 
   return wallet.signTransaction(tx, decodeTxMessageFunction);
 }
