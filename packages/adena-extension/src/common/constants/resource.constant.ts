f98130b97f2ac47e96bd7faa823e4b319f3755b6
jinoosss <112360739+jinoosss@users.noreply.github.com>
2024-09-26 12:21:12 +0900

    chore: update gno docs link url (#591)

--- packages/adena-extension/src/common/constants/resource.constant.ts
+++ packages/adena-extension/src/common/constants/resource.constant.ts
@@ -6,5 +6,5 @@ export const ADENA_SETUP_AIRGAP_HELP_PAGE =
 export const HARDWARE_WALLET_LEARN_PAGE =
   'https://docs.adena.app/user-guide/sign-in/connect-hardware-wallet' as const;
 export const GNO_CLI_HELP_PAGE =
-  'https://docs.gno.land/getting-started/local-setup/working-with-key-pairs' as const;
+  'https://docs.gno.land/gno-tooling/cli/gnokey/working-with-key-pairs' as const;
 export const SCANNER_URL = 'https://gnoscan.io' as const;
