f05c3bd26dc0358881c1de5e79e896e464d85a8b
jinoosss <jwchoi@onbloc.xyz>
2024-10-23 01:47:00 +0900

    feat: add memorized transfer page data states

--- packages/adena-extension/src/states/index.ts
+++ packages/adena-extension/src/states/index.ts
@@ -1,6 +1,7 @@
-export * as WalletState from './wallet';
-export * as ExploreState from './explore';
+export * as BalanceState from './balance';
 export * as CommonState from './common';
+export * as ExploreState from './explore';
 export * as NetworkState from './network';
 export * as TokenState from './token';
-export * as BalanceState from './balance';
+export * as TransferState from './transfer';
+export * as WalletState from './wallet';
