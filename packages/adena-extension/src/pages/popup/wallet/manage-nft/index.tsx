d094818d82be1bd86231a3b13b101984b393b1d8
jinoosss <jwchoi@onbloc.xyz>
2024-10-31 01:49:45 +0900

    feat: add nft page components

---
+++ packages/adena-extension/src/pages/popup/wallet/manage-nft/index.tsx
@@ -0,0 +1,89 @@
+import BigNumber from 'bignumber.js';
+import React, { useCallback, useEffect, useState } from 'react';
+
+import UnknownTokenIcon from '@assets/common-unknown-token.svg';
+import { ManageTokenLayout } from '@components/pages/manage-token-layout';
+import ManageTokenSearch from '@components/pages/manage-token/manage-token';
+import useAppNavigate from '@hooks/use-app-navigate';
+import { useCurrentAccount } from '@hooks/use-current-account';
+import { useTokenBalance } from '@hooks/use-token-balance';
+import { RoutePath } from '@types';
+
+const ManageNFTContainer: React.FC = () => {
+  const { navigate, goBack } = useAppNavigate();
+  const [searchKeyword, setSearchKeyword] = useState('');
+  const [isClose, setIsClose] = useState(false);
+  const { currentAccount } = useCurrentAccount();
+  const { currentBalances, toggleDisplayOption } = useTokenBalance();
+
+  useEffect(() => {
+    if (isClose) {
+      goBack();
+    }
+  }, [isClose]);
+
+  const filterTokens = useCallback(
+    (keyword: string) => {
+      const comparedKeyword = keyword.toLowerCase();
+      const filteredTokens = currentBalances
+        .filter((token) => {
+          if (comparedKeyword === '') return true;
+          if (token.name.toLowerCase().includes(comparedKeyword)) return true;
+          if (token.symbol.toLowerCase().includes(comparedKeyword)) return true;
+          return false;
+        })
+        .map((metainfo) => {
+          return {
+            ...metainfo,
+            balanceAmount: {
+              value: BigNumber(metainfo.amount.value).toFormat(),
+              denom: metainfo.amount.denom,
+            },
+            logo: metainfo.image || `${UnknownTokenIcon}`,
+          };
+        });
+      return filteredTokens;
+    },
+    [currentBalances],
+  );
+
+  const moveTokenAddedPage = useCallback(() => {
+    navigate(RoutePath.ManageTokenAdded);
+  }, [navigate]);
+
+  const onChangeKeyword = useCallback((keyword: string) => {
+    setSearchKeyword(keyword);
+  }, []);
+
+  const onToggleActiveItem = useCallback(
+    (tokenId: string, activated: boolean) => {
+      if (!currentAccount) {
+        return;
+      }
+      const changedToken = currentBalances.find((token) => tokenId === token.tokenId);
+      if (changedToken) {
+        toggleDisplayOption(currentAccount, changedToken, activated);
+      }
+    },
+    [currentBalances],
+  );
+
+  const onClickClose = useCallback(() => {
+    setIsClose(true);
+  }, []);
+
+  return (
+    <ManageTokenLayout>
+      <ManageTokenSearch
+        keyword={searchKeyword}
+        tokens={filterTokens(searchKeyword)}
+        onClickAdded={moveTokenAddedPage}
+        onClickClose={onClickClose}
+        onChangeKeyword={onChangeKeyword}
+        onToggleActiveItem={onToggleActiveItem}
+      />
+    </ManageTokenLayout>
+  );
+};
+
+export default ManageNFTContainer;
