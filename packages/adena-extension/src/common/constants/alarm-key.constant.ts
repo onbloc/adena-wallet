c7aa2a7b65b24bf5f93edfbd39ada5eb703aaab6
jinoosss <jwchoi@onbloc.xyz>
2025-02-28 18:48:41 +0900

    fix: adjust auto lock features

--- packages/adena-extension/src/common/constants/alarm-key.constant.ts
+++ packages/adena-extension/src/common/constants/alarm-key.constant.ts
@@ -1,3 +1,13 @@
 export enum AlarmKey {
   EXPIRED_PASSWORD = 'EXPIRED_PASSWORD',
+  WAKE_ALARM = 'WAKE_ALARM',
+  WAKE_ALARM_DELAY_20S = 'WAKE_ALARM_DELAY_20S',
+  WAKE_ALARM_DELAY_40S = 'WAKE_ALARM_DELAY_40S',
 }
+
+export const SCHEDULE_ALARMS: { key: string; periodInMinutes: number }[] = [
+  { key: AlarmKey.EXPIRED_PASSWORD, periodInMinutes: 1 },
+  { key: AlarmKey.WAKE_ALARM, periodInMinutes: 1 },
+  { key: AlarmKey.WAKE_ALARM_DELAY_20S, periodInMinutes: 1 },
+  { key: AlarmKey.WAKE_ALARM_DELAY_40S, periodInMinutes: 1 },
+];
