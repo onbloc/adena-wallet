c520e1a3c49d328e66b8a0a6261fd788a87bd224
jinoosss <112360739+jinoosss@users.noreply.github.com>
2025-04-10 11:56:34 +0900

    fix: handle account mismatch errors in tx-link transactions

--- packages/adena-extension/src/common/validation/validation-transaction.ts
+++ packages/adena-extension/src/common/validation/validation-transaction.ts
@@ -1,18 +1,10 @@
 import { WalletResponseFailureType } from '@adena-wallet/sdk';
-import { isBech32Address } from '@common/utils/string-utils';
 import { InjectionMessage, InjectionMessageInstance } from '@inject/message';
 
 export const validateInjectionData = (requestData: InjectionMessage): InjectionMessage | null => {
   if (!validateInjectionTransactionType(requestData)) {
     return InjectionMessageInstance.failure(
       WalletResponseFailureType.UNSUPPORTED_TYPE,
-      {},
-      requestData?.key,
-    );
-  }
-  if (!validateInjectionTransactionMessage(requestData)) {
-    return InjectionMessageInstance.failure(
-      WalletResponseFailureType.ACCOUNT_MISMATCH,
       {},
       requestData?.key,
     );
@@ -35,7 +27,7 @@ export const validateInjectionDataWithAddress = (
   if (!validateInjectionTransactionMessageWithAddress(requestData, address)) {
     return InjectionMessageInstance.failure(
       WalletResponseFailureType.ACCOUNT_MISMATCH,
-      {},
+      { requestData, address },
       requestData?.key,
     );
   }
@@ -46,34 +38,6 @@ export const validateInjectionDataWithAddress = (
 export const validateInjectionTransactionType = (requestData: InjectionMessage): any => {
   const messageTypes = ['/bank.MsgSend', '/vm.m_call', '/vm.m_addpkg', '/vm.m_run'];
   return requestData.data?.messages.every((message: any) => messageTypes.includes(message?.type));
-};
-
-export const validateInjectionTransactionMessage = (requestData: InjectionMessage): boolean => {
-  const messages = requestData.data?.messages;
-  for (const message of messages) {
-    let address = '';
-    switch (message?.type) {
-      case '/bank.MsgSend':
-        address = message.value.from_address;
-        break;
-      case '/vm.m_call':
-        address = message.value.caller;
-        break;
-      case '/vm.m_addpkg':
-        address = message.value.creator;
-        break;
-      case '/vm.m_run':
-        address = message.value.caller;
-        break;
-      default:
-        break;
-    }
-
-    if (!isBech32Address(address)) {
-      return false;
-    }
-  }
-  return true;
 };
 
 export const validateInjectionTransactionMessageWithAddress = (
@@ -100,9 +64,18 @@ export const validateInjectionTransactionMessageWithAddress = (
         break;
     }
 
-    if (messageAddress !== address) {
+    if (!validateCallerAddress(messageAddress, address)) {
       return false;
     }
   }
+
   return true;
+};
+
+const validateCallerAddress = (messageAddress: any, currentAddress: string): boolean => {
+  if (messageAddress === '') {
+    return true;
+  }
+
+  return messageAddress === currentAddress;
 };
