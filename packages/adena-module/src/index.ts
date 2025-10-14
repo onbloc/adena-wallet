9621f5044b64e4687a701a376d4ecdbb21613e05
jinoosss <112360739+jinoosss@users.noreply.github.com>
2025-10-14 18:50:54 +0900

    chore: update gno js libraries (#778)

--- packages/adena-module/src/index.ts
+++ packages/adena-module/src/index.ts
@@ -1,7 +1,6 @@
 export * from './crypto';
 export * from './encoding';
 export * from './ledger';
-export * from './libs';
 export * from './math';
 export * from './utils';
 export * from './wallet';
