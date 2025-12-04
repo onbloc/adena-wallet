b7ef92632fa26c4b09a0cbcc9a01720555bde042
yjin <devyzyn@gmail.com>
2025-12-04 18:05:27 +0900

    feat: [ADN-733] CreateMultisigAccount Response

--- packages/adena-extension/src/inject/types/multisig.ts
+++ packages/adena-extension/src/inject/types/multisig.ts
@@ -1,4 +1,13 @@
+import { AdenaResponse } from './common';
+
 export interface CreateMultisigAccountParams {
   signers: string[];
   threshold: number;
 }
+
+export type CreateMultisigAccountResponseData = {
+  multisigAddress: string;
+  multisigAddressBytes: Uint8Array;
+};
+
+export type CreateMultisigAccountResponse = AdenaResponse<CreateMultisigAccountResponseData>;
