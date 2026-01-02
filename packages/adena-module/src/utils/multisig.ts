83f48affabecf4f07af320d3883511507b11da9e
jinoosss <112360739+jinoosss@users.noreply.github.com>
2026-01-02 18:45:08 +0900

    refactor: simplify multisig public key creation by updating parameters and reintroducing encoding function

--- packages/adena-module/src/utils/multisig.ts
+++ packages/adena-module/src/utils/multisig.ts
@@ -1,9 +1,50 @@
-import { Any, defaultAddressPrefix } from '@gnolang/tm2-js-client';
-import { sha256 } from '../crypto';
+import { Any, defaultAddressPrefix, PubKeySecp256k1 } from '@gnolang/tm2-js-client';
 import { PubKeyMultisig } from '@gnolang/tm2-js-client/bin/proto/tm2/multisig';
-import { PubKeySecp256k1 } from '@gnolang/tm2-js-client';
-import { toBech32 } from '../encoding';
 import Long from 'long';
+import { sha256 } from '../crypto';
+import { toBech32 } from '../encoding';
+
+export interface Pubkey {
+  '@type': string;
+  value: Uint8Array;
+}
+
+/**
+ * Convert Proto object to Amino and generate multisig public key
+ * @param multisigPubKey - PubKeyMultisig object
+ * @param addressPrefix - Address prefix (default: 'g')
+ * @returns { address, publicKey } - Bech32 address and Amino encoded public key
+ */
+export function createMultisigPublicKey(
+  threshold: number,
+  publicKeys: Pubkey[],
+  addressPrefix: string = defaultAddressPrefix,
+): { address: string; publicKey: Uint8Array } {
+  const publicKeysAny = publicKeys.map((pk) => {
+    return Any.create({
+      type_url: pk['@type'],
+      value: pk.value,
+    });
+  });
+
+  const aminoEncoded = encodeMultisigPubKey(threshold, publicKeysAny);
+  const hash = sha256(aminoEncoded);
+  const addressBytes = hash.slice(0, 20);
+  const address = toBech32(addressPrefix, addressBytes);
+
+  return {
+    address,
+    publicKey: aminoEncoded,
+  };
+}
+
+export function convertMessageToAmino(msg: any): { type: string; value: any } {
+  if (msg.type && msg.value) {
+    return msg;
+  }
+  const { '@type': type, ...value } = msg;
+  return { type, value };
+}
 
 /**
  * Multisig Public Key encoding using Protobuf library
@@ -31,33 +72,4 @@ function encodeMultisigPubKey(threshold: number, pubkeys: Any[]): Uint8Array {
   });
 
   return Any.encode(result).finish();
-}
-
-/**
- * Convert Proto object to Amino and generate multisig public key
- * @param multisigPubKey - PubKeyMultisig object
- * @param addressPrefix - Address prefix (default: 'g')
- * @returns { address, publicKey } - Bech32 address and Amino encoded public key
- */
-export function createMultisigPublicKey(
-  multisigPubKey: PubKeyMultisig,
-  addressPrefix: string = defaultAddressPrefix,
-): { address: string; publicKey: Uint8Array } {
-  const aminoEncoded = encodeMultisigPubKey(multisigPubKey.k.toNumber(), multisigPubKey.pub_keys);
-  const hash = sha256(aminoEncoded);
-  const addressBytes = hash.slice(0, 20);
-  const address = toBech32(addressPrefix, addressBytes);
-
-  return {
-    address,
-    publicKey: aminoEncoded,
-  };
-}
-
-export function convertMessageToAmino(msg: any): { type: string; value: any } {
-  if (msg.type && msg.value) {
-    return msg;
-  }
-  const { '@type': type, ...value } = msg;
-  return { type, value };
 }
