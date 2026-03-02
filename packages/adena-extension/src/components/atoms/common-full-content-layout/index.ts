844cca480d4e45920fed5c9f66ed0065cf088ccf
jinoosss <112360739+jinoosss@users.noreply.github.com>
2026-03-02 17:07:31 +0900

    fix: adjust layout and styling in transfer summary component (#806)

--- packages/adena-extension/src/components/atoms/common-full-content-layout/index.ts
+++ packages/adena-extension/src/components/atoms/common-full-content-layout/index.ts
@@ -5,4 +5,5 @@ export const CommonFullContentLayout = styled.section`
   ${mixins.flex({ align: 'normal', justify: 'normal' })};
   position: relative;
   width: 100%;
+  height: calc(100vh - 48px);
 `;
