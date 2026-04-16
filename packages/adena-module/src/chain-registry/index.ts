117ba22817fac96ab248ec90ef50cc93452352a5
HeesungB <hezo25@naver.com>
2026-04-16 11:54:00 +0900

    refactor(chain-registry): remove global singletons from ChainRegistry and TokenRegistry

--- packages/adena-module/src/chain-registry/index.ts
+++ packages/adena-module/src/chain-registry/index.ts
@@ -2,12 +2,3 @@ export * from './types';
 export * from './registry';
 export * from './entries/gno';
 export * from './entries/atomone';
-
-import { ChainRegistryImpl } from './registry';
-import { GNO_CHAINS } from './entries/gno';
-import { ATOMONE_CHAINS } from './entries/atomone';
-
-export const defaultChainRegistry = new ChainRegistryImpl();
-
-const ALL_CHAINS = [...GNO_CHAINS, ...ATOMONE_CHAINS];
-ALL_CHAINS.forEach((chain) => defaultChainRegistry.register(chain));
